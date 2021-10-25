a025158d60e8ba59132f1efff78771a130782153
Michael Frank <frank.522@osu.edu>
2021-10-24 21:39:21 -0400

    changed product id values

--- src/app/products.ts
+++ src/app/products.ts
@@ -9,7 +9,7 @@ export interface Product {
   
   export const products = [
     {
-      id: 1,
+      id: 232,
       name: 'Aoenium',
       price: 7.99,
       description:
@@ -18,7 +18,7 @@ export interface Product {
       img_src: "assets/imgs/aeonium.jpg",
     },
     {
-      id: 2,
+      id: 321,
       name: 'Kalanchoe',
       price: 6.99,
       description:
@@ -28,7 +28,7 @@ export interface Product {
  
     },
     {
-      id: 3,
+      id: 513,
       name: 'Senecio',
       price: 2.99,
       description: 'Senecio is a genus of flowering plants that contains more than 1000 species of groundsels, ragworts, and dusty miller.',
@@ -37,7 +37,7 @@ export interface Product {
 
     },
     {
-      id: 4,
+      id: 977,
       name: 'Sempervivum',
       price: 6.35,
       description: 'Sempervivum is a genus of about 40 species of flowering plants that are commonly known as Houseleeks or Hen and Chicks.',
@@ -46,13 +46,12 @@ export interface Product {
 
     },
     {
-      id: 5,
+      id: 397,
       name: 'Crassula',
       price: 7.49,
       description: 'Crassula is a genus of succulent plants that includes more than 350 species native to many different parts of the world.',
       quantity: 0,
       img_src: "assets/imgs/crassula.jpg",
-
     },
   ];
 
