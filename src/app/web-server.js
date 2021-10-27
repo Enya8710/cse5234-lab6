94e04baad8afcdfb7b6a8172f805389e22da3a1c
enyu <enyu@enyus-MacBook-Pro.local>
2021-10-26 21:27:58 -0400

    modify web-server.js

--- src/app/web-server.js
+++ src/app/web-server.js
@@ -9,7 +9,19 @@ const express = require('express');
 const app = express();
 app.use(requireHTTPS);
 
-app.use(express.static(`./dist/lab5`));
+app.use(function (req, res, next,express){
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  res.header("Access-Control-Allow-Origin", "https://cse5234-order-microservice.herokuapp.com/OrderMicroservice/Order"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  // Pass to next layer of middleware
+  next();
+  express.static(`./dist/lab5`) 
+});
 
 app.get(`/*`, function(req, res) {
   res.sendFile(`index.html`, {root: 'dist/lab5/'});
